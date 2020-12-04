7347097a5bb7ccc06510b55fe2ad863bd546233d
baskar-create <baskarduke@gmail.com>
2020-12-04 07:25:55 -0800

    final

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
   
     return (
     <div className="App">
+     <h1>Monster Rolodex</h1>
      <Search placeholder="search" handler={this.handleChange} />
     <CardList monsters={filteredMonsters}/>
     </div>
